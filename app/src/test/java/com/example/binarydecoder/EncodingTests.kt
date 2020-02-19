22fc04bda9cb902c3b52ea754d6d9e35e7c99e75
PontusEdbor <pontus.edbor@gmail.com>
2020-02-19 12:06:10 +0100

    Added tests and fixed an edge case in Encoder.encode

---
+++ app/src/test/java/com/example/binarydecoder/EncodingTests.kt
@@ -0,0 +1,25 @@
+package com.example.binarydecoder
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class EncodingTests {
+    @Test
+    fun spaceEncodesCorrect() {//Space should be ascii code 32
+        assertEquals("00100000", Encoder().toAscii(" "))
+    }
+    @Test
+    fun aEncodesCorrect() {//Space should be ascii code 32
+        assertEquals("01100001", Encoder().toAscii("a"))
+    }
+    @Test
+    fun whatIsToChar(){
+        assertEquals(32,' '.toInt())
+    }
+}
